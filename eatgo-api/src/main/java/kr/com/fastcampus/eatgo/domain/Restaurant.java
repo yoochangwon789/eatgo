db956d5ba9a46f408069c4386a2989c34f05d479
changwon <ckddnjs554@naver.com>
2020-12-21 20:53:32 +0900

    Restaurant_getName_method

--- eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/Restaurant.java
@@ -1,4 +1,8 @@
 package kr.com.fastcampus.eatgo.domain;
 
 public class Restaurant {
+
+    public String getName() {
+        return "";
+    }
 }
--- eatgo-api/src/test/java/kr/com/fastcampus/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/com/fastcampus/eatgo/domain/RestaurantTests.java
@@ -2,6 +2,8 @@ package kr.com.fastcampus.eatgo.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTests {
@@ -9,5 +11,6 @@ class RestaurantTests {
     @Test
     public void creation() {
         Restaurant restaurant = new Restaurant();
+        assertThat(restaurant.getName(), is("Bob zip"));
     }
 }
