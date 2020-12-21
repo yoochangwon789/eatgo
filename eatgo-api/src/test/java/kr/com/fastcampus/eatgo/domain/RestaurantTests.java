43b7f24d70fa97f44669b0683c4df6db938c989a
changwon <ckddnjs554@naver.com>
2020-12-21 20:50:54 +0900

    RestaurantTests_creation_method

---
+++ eatgo-api/src/test/java/kr/com/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,13 @@
+package kr.com.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant();
+    }
+}
