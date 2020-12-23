76be8a8a7e33b8ff3c9d32a0b794ed608a14a6ed
changwon <ckddnjs554@naver.com>
2020-12-23 20:51:43 +0900

    RestaurantRepository : 중복 코드 분류 findAll 메소드 생성

---
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package kr.com.fastcampus.eatgo.domain;
+
+
+import java.util.List;
+
+public class RestaurantRepository {
+
+    public List<Restaurant> findAll() {
+        return null;
+    }
+}
--- eatgo-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,6 +1,8 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
 import kr.com.fastcampus.eatgo.domain.Restaurant;
+import kr.com.fastcampus.eatgo.domain.RestaurantRepository;
+import org.apache.catalina.util.ErrorPageSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,16 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
+//        List<Restaurant> restaurants = new ArrayList<>();
+//
+//        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+//        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
 
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
     }
