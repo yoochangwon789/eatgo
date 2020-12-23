e727117d7f4afc0877ab25fb43f2b79740d205db
changwon <ckddnjs554@naver.com>
2020-12-23 21:01:51 +0900

    RestaurantRepository : findById 메서드 생성 후 중복 코드 리팩토링

--- eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/RestaurantRepository.java
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/RestaurantRepository.java
@@ -5,12 +5,21 @@ import java.util.List;
 
 public class RestaurantRepository {
 
-    public List<Restaurant> findAll() {
-        List<Restaurant> restaurants = new ArrayList<>();
+    private List<Restaurant> restaurants = new ArrayList<>();
 
+    public RestaurantRepository() {
         restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
         restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+    }
 
+    public List<Restaurant> findAll() {
         return restaurants;
+    }
+
+    public Restaurant findById(Long id) {
+        return restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .get();
     }
 }
--- eatgo-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -2,12 +2,10 @@ package kr.com.fastcampus.eatgo.interfaces;
 
 import kr.com.fastcampus.eatgo.domain.Restaurant;
 import kr.com.fastcampus.eatgo.domain.RestaurantRepository;
-import org.apache.catalina.util.ErrorPageSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,12 +22,7 @@ public class RestaurantController {
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = repository.findAll();
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        Restaurant restaurant = repository.findById(id);
 
         return restaurant;
     }
