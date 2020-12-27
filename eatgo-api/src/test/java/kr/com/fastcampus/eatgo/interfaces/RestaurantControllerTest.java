3b892c647423376f4478a451c59697b51d0713a1
changwon <ckddnjs554@naver.com>
2020-12-27 20:18:49 +0900

    RestaurantControllerTest : 객채의 DI를 위해 MockBean 가짜객체 추가

--- eatgo-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,19 +1,21 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
 import kr.com.fastcampus.eatgo.application.RestaurantService;
-import kr.com.fastcampus.eatgo.domain.MenuItemRepository;
-import kr.com.fastcampus.eatgo.domain.MenuItemRepositoryImpl;
-import kr.com.fastcampus.eatgo.domain.RestaurantRepository;
-import kr.com.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
+import kr.com.fastcampus.eatgo.domain.*;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,18 +28,21 @@ class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    // SpyBean 이라는 어노탠션을 test 코드에 의존성을 주입시켜준다.
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
-
-    @SpyBean(RestaurantService.class)
+    // 까자 객체 투입
+    @MockBean
     private RestaurantService restaurantService;
+
+    // SpyBean 이라는 어노탠션을 test 코드에 의존성을 주입시켜준다.
+    //@SpyBean(RestaurantService.class)
+    //private RestaurantService restaurantService;
 
     @Test
     public void list() throws Exception {
+        // 우리가 given 을 통해 Service 를 실행한 후에 restaurants 의 값을 리턴 받을 것이다.
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1004")))
