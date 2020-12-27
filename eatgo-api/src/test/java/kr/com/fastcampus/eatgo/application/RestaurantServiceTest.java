142afe7c1270431a3c47eee6192914c45d6987ca
changwon <ckddnjs554@naver.com>
2020-12-27 20:45:34 +0900

    RestaurantServiceTest : mockRestaurantRepository 메소드 생성(중복코드 명시)

--- eatgo-api/src/test/java/kr/com/fastcampus/eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/kr/com/fastcampus/eatgo/application/RestaurantServiceTest.java
@@ -3,20 +3,26 @@ package kr.com.fastcampus.eatgo.application;
 import kr.com.fastcampus.eatgo.domain.*;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
 
+    @Mock
     private RestaurantRepository restaurantRepository;
 
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     //@Before
@@ -25,10 +31,28 @@ public class RestaurantServiceTest {
         //restaurantService = new RestaurantService(restaurantRepository);
     //}
 
+    private void mockRestaurantRepository() {
+        // Mock 가짜 객체를 사용하기 위해 값을 넣어주고 잡아주는 기능을 하는 명시
+        MockitoAnnotations.initMocks(this);
+
+        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+
+        Restaurant restaurant2 = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurant2.addMenuItem(new MenuItem("Kimchi"));
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant2);
+    }
+
     @Test
     public void getRestaurants() {
-        restaurantRepository = new RestaurantRepositoryImpl();
-        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+        //restaurantRepository = new RestaurantRepositoryImpl();
+        //restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+
+        mockRestaurantRepository();
 
         List<Restaurant> restaurants = restaurantService.getRestaurants();
 
@@ -38,9 +62,11 @@ public class RestaurantServiceTest {
 
     @Test
     public void getRestaurant() {
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
-        restaurantService = new RestaurantService(restaurantRepository,menuItemRepository);
+        //restaurantRepository = new RestaurantRepositoryImpl();
+        //menuItemRepository = new MenuItemRepositoryImpl();
+        //restaurantService = new RestaurantService(restaurantRepository,menuItemRepository);
+
+        mockRestaurantRepository();
 
         Restaurant restaurant = restaurantService.getRestaurant(1004L);
         assertThat(restaurant.getId(), is(1004L));
