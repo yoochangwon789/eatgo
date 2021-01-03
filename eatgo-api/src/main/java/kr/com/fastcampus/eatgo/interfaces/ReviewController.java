3245be5e2f17ba68164d72ca13a7a340972fcb53
changwon <ckddnjs554@naver.com>
2021-01-03 23:14:41 +0900

    ReviewService 구현

---
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReviewController.java
@@ -0,0 +1,18 @@
+package kr.com.fastcampus.eatgo.interfaces;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+public class ReviewController {
+
+    @PostMapping("/restaurants/{restaurantId}/reviews")
+    public ResponseEntity<?> create() throws URISyntaxException {
+        return ResponseEntity.created(new URI("/restaurants/1/reviews/1"))
+                .body("{}");
+    }
+}
