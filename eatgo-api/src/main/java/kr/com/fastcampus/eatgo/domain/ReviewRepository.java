5d0b3e7230ef7952135a539f22c3005c664cb3f3
changwon <ckddnjs554@naver.com>
2021-01-05 22:55:15 +0900

    ReviewRepository : findAllByRestaurantId 파라미터 변수 이름 변경

--- eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/ReviewRepository.java
+++ eatgo-api/src/main/java/kr/com/fastcampus/eatgo/domain/ReviewRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 
-    List<Review> findAllByRestaurantId(Long restaurant);
+    List<Review> findAllByRestaurantId(Long restaurantId);
 
     Review save(Review review);
 }
