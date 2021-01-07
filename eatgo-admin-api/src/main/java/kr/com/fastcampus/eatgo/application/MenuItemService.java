cbc2c17cb5fae04778b884ff7567a5cb198f63b5
changwon <ckddnjs554@naver.com>
2021-01-07 21:13:48 +0900

    eatgo-common : eatgo-admin-api 프로젝트와 연동

--- eatgo-admin-api/src/main/java/kr/com/fastcampus/eatgo/application/MenuItemService.java
+++ eatgo-admin-api/src/main/java/kr/com/fastcampus/eatgo/application/MenuItemService.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Service
 public class MenuItemService {
 
+    @Autowired
     private MenuItemRepository menuItemRepository;
 
     public MenuItemService(MenuItemRepository menuItemRepository) {
