766e5cfc2dec1f920e79c6188a0ee389637d9d52
changwon <ckddnjs554@naver.com>
2021-01-07 20:25:25 +0900

    프로젝스 분리 작업

--- eatgo-admin-api/src/main/java/kr/com/fastcampus/eatgo/EatgoAdminApplication.java
+++ eatgo-admin-api/src/main/java/kr/com/fastcampus/eatgo/EatgoAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EatgoApplication {
+public class EatgoAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EatgoApplication.class, args);
+        SpringApplication.run(EatgoAdminApplication.class, args);
     }
 
 }
