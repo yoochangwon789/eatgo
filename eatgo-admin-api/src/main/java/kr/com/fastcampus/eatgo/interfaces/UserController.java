fb8d20c4cfe3422d8e55c011129c910adefc3715
changwon <ckddnjs554@naver.com>
2021-01-16 20:03:06 +0900

    Feat : User table 생성

--- eatgo-admin-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/UserController.java
+++ eatgo-admin-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/UserController.java
@@ -1,6 +1,18 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
+import kr.com.fastcampus.eatgo.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/users")
+    public List<User> list() {
+        return null;
+    }
 
     // 1. User list
     // 2. User create -> 회원 가입
