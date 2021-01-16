b33f33cd964ce506b77a4d5635671587302cf2f8
changwon <ckddnjs554@naver.com>
2021-01-16 21:17:08 +0900

    Feat : UserRepository 구현

--- eatgo-admin-api/src/test/java/kr/com/fastcampus/eatgo/application/UserServiceTests.java
+++ eatgo-admin-api/src/test/java/kr/com/fastcampus/eatgo/application/UserServiceTests.java
@@ -1,22 +1,46 @@
 package kr.com.fastcampus.eatgo.application;
 
+import kr.com.fastcampus.eatgo.domain.CategoryRepository;
 import kr.com.fastcampus.eatgo.domain.User;
+import kr.com.fastcampus.eatgo.domain.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 class UserServiceTests {
 
     private UserService userService;
 
+    @Mock
+    private UserRepository userRepository;
+
+    public void setUserServiceUp() {
+        MockitoAnnotations.initMocks(this);
+        userService = new UserService(userRepository);
+    }
+
     @Test
     public void getUsers() {
-        List<User> users = userService.getUsers();
+        setUserServiceUp();
 
+        List<User> mockUsers = new ArrayList<>();
+        mockUsers.add(User.builder()
+                .email("tester@exmaple.com")
+                .name("tester")
+                .level(1L)
+                .build());
+
+        given(userRepository.findAll()).willReturn(mockUsers);
+
+        List<User> users = userService.getUsers();
         User user = users.get(0);
 
         assertThat(user.getName(), is("tester"));
