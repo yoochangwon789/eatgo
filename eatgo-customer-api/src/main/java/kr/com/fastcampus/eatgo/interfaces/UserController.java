31a8c1fc29de5a3c9f190f14290223994c78b60a
changwon <ckddnjs554@naver.com>
2021-01-17 22:03:46 +0900

    Feat : UserController PostMapping 구현

--- eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/UserController.java
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/UserController.java
@@ -1,4 +1,18 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
 public class UserController {
+
+    @PostMapping("/users")
+    public ResponseEntity<?> create() throws URISyntaxException {
+        String url = "/users/1004";
+        return ResponseEntity.created(new URI(url)).body("{}");
+    }
 }
