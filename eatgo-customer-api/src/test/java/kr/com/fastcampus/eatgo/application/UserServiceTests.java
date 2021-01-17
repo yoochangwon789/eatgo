5fb730df3d7c6170910923a999959e1c4361e964
changwon <ckddnjs554@naver.com>
2021-01-18 01:49:00 +0900

    Feat : UserServiceTests email 중복 결과 예외처리

--- eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/application/UserServiceTests.java
+++ eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/application/UserServiceTests.java
@@ -33,4 +33,17 @@ class UserServiceTests {
 
         verify(userRepository).save(any());
     }
+
+    @Test
+    public void registerUserWithExistedEmail() {
+        setUserServiceUp();
+
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        userService.registerUser(email, name, password);
+
+        verify(userRepository).save(any());
+    }
 }
