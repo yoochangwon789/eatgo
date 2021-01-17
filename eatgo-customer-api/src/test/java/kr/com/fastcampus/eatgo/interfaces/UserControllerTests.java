ae62df8434e9239c3e0202d6414fe958eb2c67e2
changwon <ckddnjs554@naver.com>
2021-01-17 22:00:44 +0900

    Feat : UserControllerTests create 부분 테스트 코드 구현

--- eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/UserControllerTests.java
+++ eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/UserControllerTests.java
@@ -1,8 +1,34 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
 class UserControllerTests {
 
+    // E-mail, Name, Password
+    // 201
 
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void create() throws Exception {
+        mvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"email\":\"tester@example.com\",\"name\":\"Tester\",\"password\":\"test\"}\n"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "/users/1004"));
+    }
 }
