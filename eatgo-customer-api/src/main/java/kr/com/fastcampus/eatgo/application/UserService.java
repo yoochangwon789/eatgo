075466a1157f3ad4dbff560953c79b20e2506e99
changwon <ckddnjs554@naver.com>
2021-01-18 01:45:34 +0900

    Feat : UserService PasswordEncoder 추가 password 암호와 작업

--- eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/application/UserService.java
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/application/UserService.java
@@ -3,6 +3,8 @@ package kr.com.fastcampus.eatgo.application;
 import kr.com.fastcampus.eatgo.domain.User;
 import kr.com.fastcampus.eatgo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,10 +18,13 @@ public class UserService {
     }
 
     public User registerUser(String email, String name, String password) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = hash(password);
+
         User user = User.builder()
                 .email(email)
                 .name(name)
-                .password(password)
+                .password(encodedPassword)
                 .level(1L)
                 .build();
 
