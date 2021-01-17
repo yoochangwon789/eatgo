d3f51627049844fef485938270fb8f0e5a774134
changwon <ckddnjs554@naver.com>
2021-01-18 02:01:31 +0900

    Feat : UserService email 중복 결과 예외 처리 구현

--- eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/application/UserService.java
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/application/UserService.java
@@ -2,10 +2,13 @@ package kr.com.fastcampus.eatgo.application;
 
 import kr.com.fastcampus.eatgo.domain.User;
 import kr.com.fastcampus.eatgo.domain.UserRepository;
+import kr.com.fastcampus.eatgo.interfaces.EmailExistedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,6 +21,11 @@ public class UserService {
     }
 
     public User registerUser(String email, String name, String password) {
+        Optional<User> existed = userRepository.findByEmail(email);
+        if (existed.isPresent()) {
+            throw new  EmailExistedException(email);
+        }
+
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(password);
 
