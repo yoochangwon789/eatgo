9349ab43b848b5412d59b06e4e985908247e61a7
changwon <ckddnjs554@naver.com>
2021-01-19 19:25:20 +0900

    Feat : SessionController PostMapping 구현

--- eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/SessionController.java
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/SessionController.java
@@ -1,4 +1,19 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
 public class SessionController {
+
+    @PostMapping("/session")
+    public ResponseEntity<?> create() throws URISyntaxException {
+        String url = "/session";
+
+        return ResponseEntity.created(new URI(url)).body("{}");
+    }
 }
