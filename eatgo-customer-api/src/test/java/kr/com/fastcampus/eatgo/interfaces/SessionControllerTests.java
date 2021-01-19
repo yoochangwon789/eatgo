68c33ea2ffb21d5afe1248ab7e942e808e628932
changwon <ckddnjs554@naver.com>
2021-01-19 19:19:49 +0900

    TDD : SessionControllerTests 를 위한 userService 기반 테스트 코드 구성

--- eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/SessionControllerTests.java
+++ eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/SessionControllerTests.java
@@ -1,7 +1,45 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
-import static org.junit.jupiter.api.Assertions.*;
+import kr.com.fastcampus.eatgo.application.UserService;
+import kr.com.fastcampus.eatgo.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SessionController.class)
 class SessionControllerTests {
 
+    // E-mail, Name, Password
+    // 201
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void create() throws Exception {
+        mvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"email\":\"tester@example.com\",\"name\":\"Tester\",\"password\":\"test\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "/users/1004"));
+
+    }
 }
