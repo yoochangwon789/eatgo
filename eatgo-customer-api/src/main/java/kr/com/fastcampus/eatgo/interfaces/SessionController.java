a791e1db8387fc3206873c8e8e80575e823e93bd
changwon <ckddnjs554@naver.com>
2021-01-19 19:42:43 +0900

    Feat : SessionController 에 UserService 사용과 RequestBody 타입 설정

--- eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/SessionController.java
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/SessionController.java
@@ -1,7 +1,11 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
+import kr.com.fastcampus.eatgo.application.UserService;
+import kr.com.fastcampus.eatgo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -10,10 +14,18 @@ import java.net.URISyntaxException;
 @RestController
 public class SessionController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/session")
-    public ResponseEntity<SessionDto> create() throws URISyntaxException {
+    public ResponseEntity<SessionDto> create(@RequestBody User resource) throws URISyntaxException {
         String accessToken = "ACCESSTOKEN";
         String url = "/session";
+
+        String email = resource.getEmail();
+        String password = resource.getPassword();
+
+        userService.authenticate(email, password);
 
         return ResponseEntity.created(new URI(url)).body(SessionDto.builder()
                 .accessToken(accessToken)
