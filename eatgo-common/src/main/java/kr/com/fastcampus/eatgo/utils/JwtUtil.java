8b72af59b008350f395cee1ed57ea74b03980a01
changwon <ckddnjs554@naver.com>
2021-01-21 01:15:45 +0900

    Feat : JwtUtil token signature 와 key 값 설정

--- eatgo-common/src/main/java/kr/com/fastcampus/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/kr/com/fastcampus/eatgo/utils/JwtUtil.java
@@ -1,7 +1,11 @@
 package kr.com.fastcampus.eatgo.utils;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
 
 @Component
 public class JwtUtil {
@@ -9,9 +13,12 @@ public class JwtUtil {
     public String createToken(Long userId, String name) {
         //TODO : JJWT 사용!
 
+        Key key = Keys.hmacShaKeyFor("secret".getBytes());
+
         String token = Jwts.builder()
                 .claim("userId", userId)
                 .claim("name", name)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
         return token;
