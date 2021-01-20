17d307749bdbce524607503be95b1f2d2bf3a052
changwon <ckddnjs554@naver.com>
2021-01-21 01:20:29 +0900

    Feat : JwtUtil @Component 삭제

--- eatgo-common/src/main/java/kr/com/fastcampus/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/kr/com/fastcampus/eatgo/utils/JwtUtil.java
@@ -3,17 +3,14 @@ package kr.com.fastcampus.eatgo.utils;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
-@Component
 public class JwtUtil {
 
     public String createToken(Long userId, String name) {
-        //TODO : JJWT 사용!
-
-        Key key = Keys.hmacShaKeyFor("secret".getBytes());
+        String secret = "12345678901234567890123456789012";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
 
         String token = Jwts.builder()
                 .claim("userId", userId)
