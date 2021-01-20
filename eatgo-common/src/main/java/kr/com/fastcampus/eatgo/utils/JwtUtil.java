9e714a7b09abae42214965a89b25f8337e78e73a
changwon <ckddnjs554@naver.com>
2021-01-21 01:13:13 +0900

    Feat : JwtUtil token claim 추가 작업

--- eatgo-common/src/main/java/kr/com/fastcampus/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/kr/com/fastcampus/eatgo/utils/JwtUtil.java
@@ -1,13 +1,19 @@
 package kr.com.fastcampus.eatgo.utils;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    public String createToken(Long id, String name) {
+    public String createToken(Long userId, String name) {
         //TODO : JJWT 사용!
 
-        return "header.payload.signature";
+        String token = Jwts.builder()
+                .claim("userId", userId)
+                .claim("name", name)
+                .compact();
+
+        return token;
     }
 }
