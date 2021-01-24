8c43a1b0fe71167e944a57f7c1df7fb7f9d41765
changwon <ckddnjs554@naver.com>
2021-01-24 23:13:40 +0900

    Feat : doFilterInternal 구현 구성

--- eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/filters/JwtAuthenticationFilter.java
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/filters/JwtAuthenticationFilter.java
@@ -4,6 +4,12 @@ import kr.com.fastcampus.eatgo.utils.JwtUtil;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
     private JwtUtil jwtUtil;
@@ -14,4 +20,16 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     }
 
     // TODO : 실제로 JWT 분석 필요함.
+    // 필터가 무슨 요청을 할 경우 메서드를 실행하도록 하는데 나는 필터의 관한 메서드를 구현하도록 함
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws IOException, ServletException {
+        // 다음 작업으로 계속 연결시키는 구현
+        // TODO : JWT
+        chain.doFilter(request, response);
+    }
 }
