31e34c4f6a571c664292422319027a6f9948fa16
changwon <ckddnjs554@naver.com>
2021-01-26 00:04:39 +0900

    Feat : eatgo-login-api 파일 셋팅 및 수정

--- eatgo-login-api/src/main/java/kr/com/fastcampus/eatgo/application/UserService.java
+++ eatgo-login-api/src/main/java/kr/com/fastcampus/eatgo/application/UserService.java
@@ -3,7 +3,6 @@ package kr.com.fastcampus.eatgo.application;
 import kr.com.fastcampus.eatgo.domain.User;
 import kr.com.fastcampus.eatgo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,21 +19,14 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User registerUser(String email, String name, String password) {
-//        Optional<User> existed = userRepository.findByEmail(email);
-//        if (existed.isPresent()) {
-//            throw new EmailExistedException(email);
-//        }
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EmailNotExistedException(email));
 
-        String encodedPassword = passwordEncoder.encode(password);
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new PasswordWrongException();
+        }
 
-        User user = User.builder()
-                .email(email)
-                .name(name)
-                .password(encodedPassword)
-                .level(1L)
-                .build();
-
-        return userRepository.save(user);
+        return user;
     }
 }
--- eatgo-login-api/src/test/java/kr/com/fastcampus/eatgo/application/UserServiceTests.java
+++ eatgo-login-api/src/test/java/kr/com/fastcampus/eatgo/application/UserServiceTests.java
@@ -1,12 +1,18 @@
 package kr.com.fastcampus.eatgo.application;
 
+import kr.com.fastcampus.eatgo.domain.User;
 import kr.com.fastcampus.eatgo.domain.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 class UserServiceTests {
@@ -25,34 +31,21 @@ class UserServiceTests {
     }
 
     @Test
-    public void registerUser() {
+    public void authenticateWithValidAttributes() {
         setUserServiceUp();
 
         String email = "tester@example.com";
-        String name = "Tester";
         String password = "test";
 
-        userService.registerUser(email, name, password);
+        User mockUser = User.builder().email(email).build();
 
-        verify(userRepository).save(any());
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(mockUser));
+        given(passwordEncoder.matches(any(), any())).willReturn(true);
+
+        User user = userService.authenticate(email, password);
+
+        assertThat(user.getEmail(), is(email));
     }
-
-//    @Test(expected = EmailExistedException.class)
-//    public void registerUserWithExistedEmail() {
-//        setUserServiceUp();
-//
-//        String email = "tester@example.com";
-//        String name = "Tester";
-//        String password = "test";
-//
-//        User user = User.builder().build();
-//
-//        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
-//
-//        userService.registerUser(email, name, password);
-//
-//        verify(userRepository, never()).save(any());
-//    }
 
 //    @Test
 //    public void authenticateWithNotExistedEmail() {
