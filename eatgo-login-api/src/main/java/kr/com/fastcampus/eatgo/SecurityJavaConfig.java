05230c3fa214c032e7025ef8af7b8ae4131b6f47
changwon <ckddnjs554@naver.com>
2021-01-26 00:20:33 +0900

    Feat : eatgo-login-api Config 설정

--- eatgo-login-api/src/main/java/kr/com/fastcampus/eatgo/SecurityJavaConfig.java
+++ eatgo-login-api/src/main/java/kr/com/fastcampus/eatgo/SecurityJavaConfig.java
@@ -4,10 +4,14 @@ import kr.com.fastcampus.eatgo.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +19,15 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${jwt.secret}")
     private String secret;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
