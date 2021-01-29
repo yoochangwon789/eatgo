d2a410777a4c7651e69e071121e40dfb5287ece4
changwon <ckddnjs554@naver.com>
2021-01-29 20:36:40 +0900

    Feat : Table 예약 시스템을 위해 ReservationController 생성

---
+++ eatgo-customer-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
@@ -0,0 +1,13 @@
+package kr.com.fastcampus.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+
+    @PostMapping
+    public void create() {
+         // TODO : 구현 예정
+    }
+}
---
+++ eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/ReservationControllerTests.java
@@ -0,0 +1,13 @@
+package kr.com.fastcampus.eatgo.interfaces;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationControllerTests {
+
+    @Test
+    public void create() {
+
+    }
+}
