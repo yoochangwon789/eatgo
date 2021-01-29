860451e703e23ab627381ec845bcfd3e4089da64
changwon <ckddnjs554@naver.com>
2021-01-29 20:49:01 +0900

    Feat : ReservationService 추가 작업

--- eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/ReservationControllerTests.java
+++ eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/ReservationControllerTests.java
@@ -1,12 +1,15 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
+import kr.com.fastcampus.eatgo.application.ReservationService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.sun.javaws.JnlpxArgs.verify;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,10 +21,14 @@ class ReservationControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ReservationService reservationService;
+
     @Test
     public void create() throws Exception {
         mvc.perform(post("/restaurants/1004/reservations"))
                 .andExpect(status().isCreated());
 
+        verify(reservationService).addReservation(userId, name, date, time, partySize);
     }
 }
