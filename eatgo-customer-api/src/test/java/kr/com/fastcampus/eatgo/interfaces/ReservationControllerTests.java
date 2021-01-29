5fa6f02f534ae08314e1c88a15111bc5419ff134
changwon <ckddnjs554@naver.com>
2021-01-29 20:41:18 +0900

    TDD : postMapping create 부분 구현 부 작성

--- eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/ReservationControllerTests.java
+++ eatgo-customer-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/ReservationControllerTests.java
@@ -1,13 +1,26 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReservationController.class)
 class ReservationControllerTests {
 
-    @Test
-    public void create() {
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    public void create() throws Exception {
+        mvc.perform(post("/restaurants/1/reservations"))
+                .andExpect(status().isCreated());
     }
 }
