5951b1eabd1682b910c362c1e3e0f675ca62cb5d
changwon <ckddnjs554@naver.com>
2021-02-04 23:15:15 +0900

    TDD : ReservationControllerTests list 테스트 코드 구현 get 방식으로

---
+++ eatgo-restaurant-api/src/test/java/kr/com/fastcampus/eatgo/interfaces/ReservationControllerTests.java
@@ -0,0 +1,33 @@
+package kr.com.fastcampus.eatgo.interfaces;
+
+import kr.com.fastcampus.eatgo.application.ReservationService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReservationController.class)
+class ReservationControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ReservationService reservationService;
+
+    @Test
+    public void list() throws Exception {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwMDQsIm5hbWUiOiJKb2huIn0.8hm6ZOJykSINHxL-rf0yV882fApL3hyQ9-WGlJUyo2A";
+
+        mvc.perform(get("/reservations")
+                .header("Authorization", "Bearer" + token))
+                .andExpect(status().isOk());
+    }
+}
