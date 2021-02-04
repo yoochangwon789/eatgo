cc21a22840e48c6388671a051fd3b945a853f4fc
changwon <ckddnjs554@naver.com>
2021-02-04 23:05:59 +0900

    TDD : restaurantApiApplicationTests 스프링 부트 테스트 로드 생성

---
+++ eatgo-restaurant-api/src/test/java/kr/com/fastcampus/eatgo/EatgoRestaurantApiApplicationTests.java
@@ -0,0 +1,18 @@
+package kr.com.fastcampus.eatgo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EatgoRestaurantApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        
+    }
+}
