260dcd4b212d49c7c2ecd04390af820063c569ba
changwon <ckddnjs554@naver.com>
2021-02-04 23:51:48 +0900

    Feat : JwtAuthenticationFilter 가져온 후 SecurityJavaConfig 원래대로 수정 후 TDD

--- eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/SecurityJavaConfig.java
+++ eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/SecurityJavaConfig.java
@@ -1,5 +1,6 @@
 package kr.com.fastcampus.eatgo;
 
+import kr.com.fastcampus.eatgo.filters.JwtAuthenticationFilter;
 import kr.com.fastcampus.eatgo.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,11 +23,18 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        Filter filter = new JwtAuthenticationFilter(authenticationManager(), jwtUtil());
+
         http
                 .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
-                .headers().frameOptions().disable();
+                .headers().frameOptions().disable()
+                .and()
+                .addFilter(filter)
+                .sessionManagement()
+                // 따로 세션의 대해서 관리를 하지 않고 계속해서 토큰을 받은 것을 바탕으로 filter 가 작업을 하도록 구성
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
---
+++ eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package kr.com.fastcampus.eatgo.filters;
+
+import io.jsonwebtoken.Claims;
+import kr.com.fastcampus.eatgo.utils.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+    }
+
+    // TODO : 실제로 JWT 분석 필요함.
+    // 필터가 무슨 요청을 할 경우 메서드를 실행하도록 하는데 나는 필터의 관한 메서드를 구현하도록 함
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws IOException, ServletException {
+        Authentication authentication = getAuthentication(request);
+        if (authentication != null) {
+            // 실제로 내가 쓰고있는 컨택트를 사용할 수 있게 된다.
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
+        // TODO : JWT 의 분석을 필요한 작업을 구현 doFilter 는 항상 실행이 되야 함.
+        chain.doFilter(request, response);
+    }
+
+    // 바로 에러 처리가 가능한 메서드 분리 외부에서 사용되는 Authentication 이 아니라 스프링 내부에서 사용하는 Authentication 임.
+    private Authentication getAuthentication(HttpServletRequest request) {
+        // 다음 작업으로 계속 연결시키는 구현
+        // 실제로는 헤서에서 데이터를 얻어와야함
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            return null;
+        }
+
+        // TODO : JWT 구현  --- JwtUtil 에서 claims 얻기.
+        Claims claims = jwtUtil.getClaims(token.substring("Bearer".length()));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(claims, null);
+        return authentication;
+    }
+}
