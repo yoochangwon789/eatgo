2eb0519176cabafe57106deba07ef4efb4987117
changwon <ckddnjs554@naver.com>
2021-02-04 23:03:13 +0900

    Feat : restaurantApiApplication 스프링 부트 스타터 생성

--- eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/EatgoRestaurantApiApplication.java
+++ eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/EatgoRestaurantApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EatgoCustomerApiApplication {
+public class EatgoRestaurantApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EatgoCustomerApiApplication.class, args);
+        SpringApplication.run(EatgoRestaurantApiApplication.class, args);
     }
 }
