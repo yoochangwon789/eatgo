3ed47ad091967672c42fccfa3f8becdd4895e26d
changwon <ckddnjs554@naver.com>
2021-02-04 23:09:18 +0900

    Feat : ReservationController GetMapping 구현 후 list 메서드 생성 반환 String

--- eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
+++ eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
@@ -6,10 +6,7 @@ import kr.com.fastcampus.eatgo.domain.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -20,23 +17,8 @@ public class ReservationController {
     @Autowired
     private ReservationService reservationService;
 
-    @PostMapping("/restaurants/{restaurantsId}/reservations")
-    public ResponseEntity<?> create(Authentication authentication,
-                                    @PathVariable Long restaurantsId,
-                                    @RequestBody Reservation resource) throws URISyntaxException {
+    @GetMapping("/reservations")
+    public String list() {
 
-        Claims claims = (Claims) authentication.getPrincipal();
-
-        Long userId = claims.get("userId", Long.class);
-        String name = claims.get("name", String.class);
-        String date = resource.getDate();
-        String time = resource.getTime();
-        Integer partySize = resource.getPartySize();
-
-        Reservation reservation = reservationService.addReservation(restaurantsId, userId, name, date, time, partySize);
-
-        String url = "/restaurants/" + restaurantsId + "/reservations/" + reservation.getId();
-
-        return ResponseEntity.created(new URI(url)).body("{}");
     }
 }
