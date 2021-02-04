e8aea2c65388cc4af282c1a802e5778ccb0b1302
changwon <ckddnjs554@naver.com>
2021-02-04 23:30:06 +0900

    Feat : ReservationController list() 메서드 구현

--- eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
+++ eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
@@ -1,15 +1,12 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
-import io.jsonwebtoken.Claims;
 import kr.com.fastcampus.eatgo.application.ReservationService;
 import kr.com.fastcampus.eatgo.domain.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 public class ReservationController {
@@ -18,7 +15,10 @@ public class ReservationController {
     private ReservationService reservationService;
 
     @GetMapping("/reservations")
-    public String list() {
-        return "";
+    public List<Reservation> list() {
+        Long restaurantId = 1004L;
+        List<Reservation> reservations = reservationService.getReservations(restaurantId);
+
+        return reservations;
     }
 }
