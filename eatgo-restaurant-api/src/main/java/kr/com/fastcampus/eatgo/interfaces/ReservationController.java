29976298860e5d300b8ce76cac0b0b3789aa4aa4
changwon <ckddnjs554@naver.com>
2021-02-04 23:47:41 +0900

    Feat : ReservationController Claims 관환 restaurantId 값으로 변경

--- eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
+++ eatgo-restaurant-api/src/main/java/kr/com/fastcampus/eatgo/interfaces/ReservationController.java
@@ -1,8 +1,10 @@
 package kr.com.fastcampus.eatgo.interfaces;
 
+import io.jsonwebtoken.Claims;
 import kr.com.fastcampus.eatgo.application.ReservationService;
 import kr.com.fastcampus.eatgo.domain.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,11 @@ public class ReservationController {
     private ReservationService reservationService;
 
     @GetMapping("/reservations")
-    public List<Reservation> list() {
-        Long restaurantId = 1004L;
+    public List<Reservation> list(Authentication authentication) {
+        Claims claims = (Claims) authentication.getPrincipal();
+
+        Long restaurantId = claims.get("restaurantId", Long.class);
+
         List<Reservation> reservations = reservationService.getReservations(restaurantId);
 
         return reservations;
